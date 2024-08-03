cc8fe93ff9f166276430b3afea32594082fd7a3e
AlexanderK <plus166202@gmail.com>
2024-08-04 01:59:09 +0500

    Solved #28

---
+++ pyramidGenerator/#28/script.js
@@ -0,0 +1,10 @@
+let character = 'Hello';
+let count = 8;
+let rows = ["Naomi", "Quincy", "CamperChan"];
+
+let pushed = rows.push("freeCodeCamp");
+
+let popped = rows.pop();
+console.log(popped);
+console.log(pushed);
+console.log(rows);
