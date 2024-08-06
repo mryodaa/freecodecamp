d3ce42009dc8204fa2f55071c8072be99a988ff8
AlexanderK <plus166202@gmail.com>
2024-08-06 23:11:44 +0500

    Solved #66

---
+++ pyramidGenerator/#66/script.js
@@ -0,0 +1,11 @@
+const character = "#";
+const count = 8;
+const rows = [];
+
+function padRow(rowNumber, rowCount) {
+    return character.repeat(rowNumber);
+}
+
+for (let i = 0; i < count; i = i + 1) {
+    rows.push(padRow())
+}
