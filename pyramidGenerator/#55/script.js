38a8ed821935de1b189828e380c218adf01e2a8b
AlexanderK <plus166202@gmail.com>
2024-08-06 22:48:25 +0500

    Solved #55

---
+++ pyramidGenerator/#55/script.js
@@ -0,0 +1,13 @@
+const character = "#";
+const count = 8;
+const rows = [];
+
+function padRow(name) {
+    return name;
+}
+
+function addTwoNumbers(a, b) {
+    return a + b;
+}
+const sum = addTwoNumbers(5, 10)
+console.log(sum)
