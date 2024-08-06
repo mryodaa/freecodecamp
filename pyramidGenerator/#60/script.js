86bb8de6e47ddc31a307e932da839cc53e6a4bb0
AlexanderK <plus166202@gmail.com>
2024-08-06 23:00:32 +0500

    Solved #60

---
+++ pyramidGenerator/#60/script.js
@@ -0,0 +1,10 @@
+const character = "#";
+const count = 8;
+const rows = [];
+
+function padRow(name) {
+    const test = "Testing";
+    console.log("This works!")
+    return test;
+    console.log("This works!")
+}
